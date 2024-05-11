1f7bdfd8161511116268bbb69366fdad3e5015f7
kuangzhibin <1520257238@qq.com>
2024-05-11 09:25:57 +0800

    fix(mono): 调整 commitlint.config scope-enum以及lerna packages
    ISSUES CLOSED: none

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,13 +1,25 @@
 const fs = require('fs')
 const path = require('path')
+const { frameworks } = require('./scripts/build/framework-config.cjs')
+
+let packages = []
+
+frameworks.forEach((framework) => {
+	const packagesPath = path.join(__dirname, './', framework, './packages')
+
+	if (fs.existsSync(packagesPath)) {
+		packages = packages.concat([...fs.readdirSync(packagesPath)])
+	}
+})
 
 module.exports = {
 	extends: ['monorepo'],
+	// 定义规则类型
 	rules: {
 		'header-max-length': [0, 'always'],
-		// scope 不允许为空，保证CHANGELOG正常写入，release的命名格式为xxx-tagname，tagname和scope保持一致
+		//  scope 不允许为空，保证CHANGELOG正常写入，release的命名格式为xxx-tagname，tagname和scope保持一致
 		'scope-empty': [2, 'never'],
-		'scope-enum': [2, 'always', [...fs.readdirSync(path.join(__dirname, 'nuxt/packages')), 'mono']],
+		'scope-enum': [2, 'always', [...new Set(packages), 'mono', ...frameworks]],
 		'type-enum': [2, 'always', ['build', 'ci', 'chore', 'feat', 'fix', 'refactor', 'style', 'test', 'config', 'docs']],
 		'close-issue-needed': [2, 'always'],
 	},
